dd8d04f3464aea15740ee0031f89e62929bafcd2
@vik_razrab <piton20091986@gmail.com>
2023-09-29 15:36:03 +0300

    add db settings and models

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=16)
+    age = models.CharField(max_length=3)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=False)
